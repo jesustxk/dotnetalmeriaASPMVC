65e8d17efceae674e8a013ff5951c64fdde66139
Jesús <Fromau5@users.noreply.github.com>
2015-04-28 18:24:14 +0200

    Creado un modelo de prueba y añadidos un par de datos.

---
+++ WebPrueba/WebPrueba/Models/Persona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebPrueba.Models
+{
+    public class Persona
+    {
+        public int PersonaID { get; set; }
+
+        [Required()]
+        public string Nombre { get; set; }
+
+        public string Apellidos { get; set; }
+
+        [Required()]
+        [Display(Name = "Dirección email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
---
+++ WebPrueba/WebPrueba/Models/WebPruebaContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebPrueba.Models
+{
+    public class WebPruebaContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public WebPruebaContext() : base("name=WebPruebaContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<WebPrueba.Models.Persona> Personas { get; set; }
+    
+    }
+}
